1bfd9dc80525d26005d8ba7e7d55edba1d3c9dc8
Daniele <208292+iamdaniele@users.noreply.github.com>
2024-07-19 18:55:43 -0700

    feat: nicer rendering of tool calls (anthropic)

--- st_utils/__init__.py
+++ st_utils/__init__.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from toolhouse.models import OpenAIStream, stream_to_chat_completion
+from types import SimpleNamespace
 
 def anthropic_stream(response):
     for chunk in response.text_stream:
@@ -10,31 +11,42 @@ def openai_stream(response, completion):
         yield chunk
         completion.add(chunk)
 
+def openai_render_tool_call(message):
+    msg = ["**Using tools**"]
+    for tool in message["tool_calls"]:
+        args = tool["function"]["arguments"] if tool["function"]["arguments"] != "{}" else ""
+        msg.append(f'```{tool["function"]["name"]}({args})```')
+
+    return "\n\n".join(msg)
+
 def print_messages(messages, provider):
     for message in messages:
         if provider == "anthropic":
-            if isinstance(message["content"], str):
-                with st.chat_message(message["role"]):
-                    st.markdown(message["content"])
-            elif isinstance(message["content"], list):
-                for m in message["content"]:
-                    if not hasattr(m, "type"):
-                        continue
-                    elif m.type == "text":
+                if isinstance(message["content"], str):
+                    with st.chat_message(message["role"]):
+                        st.markdown(message["content"])
+                elif isinstance(message["content"], list):
+                    has_tool = False
+                    msg = []
+                    for m in message["content"]:
+                        if not hasattr(m, "type"):
+                            continue
+                        elif m.type == "text":
+                            msg.append(m.text)
+                        elif m.type == "tool_use":
+                            if not has_tool:
+                                msg.append("**Using tools**")
+                            args = str(m.input) if str(m.input) != "{}" else ""
+                            msg.append(f"```{m.name}({args})```")
+                            has_tool = True
+
+                    if msg:
                         with st.chat_message(message["role"]):
-                            st.markdown(m.text)
-                    elif m.type == "tool_use":
-                        with st.chat_message("tool"):
-                            st.markdown(f"`{m.name}({m.input})`")
+                            st.markdown("\n\n".join(msg))
         else:
             if isinstance(message.get("tool_calls"), list):
                 with st.chat_message("assistant"):
-                    msg = ["**Using tools**"]
-                    for tool in message["tool_calls"]:
-                        args = tool["function"]["arguments"] if tool["function"]["arguments"] != "{}" else ""
-                        msg.append(f'```{tool["function"]["name"]}({args})```')
-
-                    st.markdown("\n\n".join(msg))
+                    st.markdown(openai_render_tool_call(message))
 
             elif message["role"] != "tool":
                 with st.chat_message(message["role"]):
@@ -46,7 +58,21 @@ def append_and_print(response, role = "assistant"):
             if st.session_state.stream:
                 r = st.write_stream(anthropic_stream(response))
                 st.session_state.messages.append({ "role": role, "content": response.get_final_message().content })
-                return response.get_final_message()
+                message = response.get_final_message()
+                
+                has_tool = False
+                msg = []
+                for m in message.content:
+                    if hasattr(m, "type") and m.type == "tool_use":
+                        has_tool = True
+                        args = str(m.input) if str(m.input) != "{}" else ""
+                        msg.append(f"```{m.name}({args})```")
+                
+                if has_tool:
+                    msg = ["**Using tools**"] + msg
+                    st.markdown("\n\n".join(msg))
+                        
+                return message
             else:
                 if response.content is not None:
                     st.session_state.messages.append({"role": role, "content": response.content})
@@ -60,13 +86,8 @@ def append_and_print(response, role = "assistant"):
                 completion = stream_to_chat_completion(stream_completion)
 
                 if completion.choices and completion.choices[0].message.tool_calls:
-                    msg = ["**Using tools**"]
-                    for tool in completion.choices[0].message.tool_calls:
-                        args = tool.function.arguments if tool.function.arguments != "{}" else ""
-                        msg.append(f'```{tool.function.name}({args})```')
-
                     st.session_state.messages.append(completion.choices[0].message.model_dump())
-                    st.markdown("\n\n".join(msg))
+                    st.markdown(openai_render_tool_call(completion.choices[0].message.to_dict()))
                 else:
                     st.session_state.messages.append({"role": role, "content": r})
                 
@@ -76,11 +97,5 @@ def append_and_print(response, role = "assistant"):
                 if (text := response.choices[0].message.content) is not None:
                     st.markdown(text)
                 elif response.choices[0].message.tool_calls:
-                    msg = ["**Using tools**"]
-                    for tool in response.choices[0].message.tool_calls:
-                        args = tool.function.arguments if tool.function.arguments != "{}" else ""
-                        msg.append(f'```{tool.function.name}({args})```')
-
-                    st.session_state.messages.append(response.choices[0].message.model_dump())
-                    st.markdown("\n\n".join(msg))
+                    st.markdown(openai_render_tool_call(response.choices[0].message.to_dict()))
                 return response
