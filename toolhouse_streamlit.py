835250e4e551a7a2018a50d955ba254a564bdd64
Daniele <208292+iamdaniele@users.noreply.github.com>
2024-07-21 16:06:24 -0700

    feat: link to tool store

--- toolhouse_streamlit.py
+++ toolhouse_streamlit.py
@@ -24,37 +24,38 @@ import dotenv
 
 dotenv.load_dotenv()
 
-st.title("💬 Toolhouse demo")
 st.logo(
     "logo.svg",
     link="https://toolhouse.ai")
 
 with st.sidebar:
+    st.title("💬 Toolhouse Playground")
     llm_choice = st.selectbox("Model", tuple(llms.keys()))
     st.session_state.stream = st.toggle("Stream responses", True)
     user = st.text_input("User", "daniele")
     st.divider()
-    t = Toolhouse(provider='anthropic')
+    t = Toolhouse(provider="anthropic")
     available_tools = t.get_tools()
+
     if not available_tools:
-        st.markdown('### No tools installed.')
-    
+        st.subheader("No tools installed")
+        st.caption("Go to the [Tool Store](https://app.toolhouse.ai/store) to install your tools.")    
     else:
-        markdown = ['### Installed tools', '']
+        st.subheader("Installed tools")
         for tool in available_tools:
-            markdown.append(' - ' + tool.get('name'))
+            st.page_link(f"https://app.toolhouse.ai/store/{tool.get("name")}", label=tool.get("name"))
         
-        st.markdown('\n'.join(markdown))
+        st.caption("\n\nManage your tools in the [Tool Store](https://app.toolhouse.ai/store).")
     
 
 llm = llms.get(llm_choice)
-st.session_state.provider = llm.get('provider')
-model = llm.get('model')
+st.session_state.provider = llm.get("provider")
+model = llm.get("model")
 
-th = Toolhouse(provider=llm.get('provider'))
-th.set_metadata('timezone', -7)
+th = Toolhouse(provider=llm.get("provider"))
+th.set_metadata("timezone", -7)
 if user:
-    th.set_metadata('id', user)
+    th.set_metadata("id", user)
 
 print_messages(st.session_state.messages, st.session_state.provider)
 
