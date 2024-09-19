ac708f50ffee5afd113361cede3427a0eb082f45
Daniele <208292+iamdaniele@users.noreply.github.com>
2024-09-19 13:55:33 -0700

    feat: cooler playground

---
+++ components/__init__.py
@@ -0,0 +1,62 @@
+import streamlit as st
+from tools import tool_prompts, generate_prompt_suggestions
+
+def sidebar():
+    with st.sidebar:
+        st.title("💬 Playground")
+        st.markdown("""
+        **Want to earn more credits?**
+
+        
+        ✨ [Join our Discord](https://discord.toolhouse.ai) and become a Toolhouse Partner
+        """)
+            
+        if not st.session_state.available_tools:
+            st.subheader("No tools installed")
+            st.caption(
+                "Go to the [Tool Store](https://app.toolhouse.ai/store) to install your tools."
+            )
+        else:
+            st.subheader("Installed tools")
+            for tool in st.session_state.available_tools:
+                tool_name = tool.get("function").get("name")
+                st.page_link(f"https://app.toolhouse.ai/store/{tool_name}", label=tool_name)
+
+            st.caption(
+                "\n\nManage your tools in the [Tool Store](https://app.toolhouse.ai/store)."
+            )
+
+def hide_hero_and_call(prompt):
+    st.session_state.hide_hero = True
+    st.session_state.prompt = prompt
+
+def hero():
+  tool_id = st.query_params.get("tool_id")
+  tool = tool_prompts.get(tool_id)
+
+  if not st.query_params.get("tool_id"):
+    with st.chat_message("user"):
+      st.markdown('<h2 style="padding:0;padding-bottom:1rem">Welcome to the Playground!</h2>', unsafe_allow_html=True)
+      st.write(
+          "This is an interactive environment where you can test Toolhouse with the Tools you installed."
+          "  \n"
+          "You can use the Playground to test out a tool, or to see how multiple tools work together."
+      )
+      
+      if st.session_state.suggestions is None:
+          st.session_state.suggestions = generate_prompt_suggestions(st.session_state.available_tools)
+      
+      st.write("Here are some suggestions to get you started:")
+      for i, suggestion in enumerate(st.session_state["suggestions"]):
+          st.markdown(f"*{suggestion}*")
+          st.button("Try this prompt", key=f"button-{i}", on_click=hide_hero_and_call, args=[suggestion])
+  else:
+      if not tool:
+          st.markdown(f"The Playground cannot load the requested tool: `{st.query_params.get("tool_id")}`. Ensure the name is correct.")
+          st.markdown("You can try a different Tool by going to [Tool Store](https://app.toolhouse.ai/store).")
+          st.stop()
+      else:
+          st.markdown(f'<div style="display:flex;"><div><img style="width:3rem;height: 3rem;margin: 1rem 2rem" src="{tool.get("logo")}" /></div><div style="flex:1"><h2 style="font-weight:700;padding: 1rem 0">Try {tool.get('title')}</h2></div>', unsafe_allow_html=True)
+          st.markdown("Click the Try button to run this prompt and see the result.")
+          st.markdown(f"*{tool.get("prompt")}*")
+          st.button("Try this prompt", key=f"button-{st.query_params.get("tool_id")}", type="primary", on_click=hide_hero_and_call, args=[tool.get("prompt")])
--- toolhouse_streamlit.py
+++ toolhouse_streamlit.py
@@ -1,19 +1,31 @@
 import streamlit as st
 from toolhouse import Toolhouse
-from llms import llms, llm_call, prepare_system_prompt
+from llms import llms, llm_call
 from http_exceptions.client_exceptions import NotFoundException
+from st_utils import print_messages, append_and_print
+from tools import tool_prompts
+from components import sidebar, hero
+import dotenv
 
-# Check for Toolhouse API key
-if not st.query_params.get("th_token"):
-    st.error("Toolhouse API Key is missing!")
-    st.markdown("To access the playground, you need to provide a Toolhouse API Key.")
-    st.markdown("Get your API Key from the [Toolhouse dashboard](https://app.toolhouse.ai/settings/api-keys)")
-    st.stop()
+th = Toolhouse(access_token=st.query_params["th_token"])
+dotenv.load_dotenv()
 
 st.set_page_config(
     page_title="Toolhouse Playground",
     page_icon="https://app.toolhouse.ai/icons/favicon.ico",
 )
+
+st.markdown("<style>@import url('https://fonts.googleapis.com/css2?family=Rethink+Sans:ital,wght@0,400..800;1,400..800&display=swap');</style>", unsafe_allow_html=True)
+st.markdown('<style>body, div, h1, h2, h3, h4, h4, h5, button,input,optgroup,select,textarea {font-family: "Rethink Sans" !important}</style>', unsafe_allow_html=True)
+tool_id = st.query_params.get("tool_id")
+tool = tool_prompts.get(tool_id)
+
+# Check for Toolhouse API key
+if not st.query_params.get("th_token"):
+    st.markdown("# Your Toolhouse API Key is missing")
+    st.markdown("To use the Playground, you need to provide a Toolhouse API Key.")
+    st.markdown("Get your API Key from the [Toolhouse dashboard](https://app.toolhouse.ai/settings/api-keys).")
+    st.stop()
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -24,54 +36,31 @@ if "stream" not in st.session_state:
 if "provider" not in st.session_state:
     st.session_state.provider = llms.get(next(iter(llms))).get("provider")
 
-from st_utils import print_messages, append_and_print
-import dotenv
+if "prompt" not in st.session_state:
+    st.session_state.prompt = None
 
-dotenv.load_dotenv()
+if "available_tools" not in st.session_state:
+    try:
+        st.session_state.available_tools = th.get_tools()
+    except NotFoundException:
+        st.session_state.available_tools = None
+
+if "hide_hero" not in st.session_state:
+    st.session_state.hide_hero = False
+    
+if "suggestions" not in st.session_state:
+    st.session_state.suggestions = None
 
 st.logo("logo.svg", link="https://toolhouse.ai")
 
 # Set some default values
 llm_choice = "GPT-4o mini"
-bundle="default"
-
-with st.sidebar:
-    st.title("💬 Playground")
-    st.markdown("""
-    **Want to earn more credits?**
-
-    
-    ✨ [Join our Discord](https://discord.toolhouse.ai) and become a Toolhouse Partner
-    """)
-
-    t = Toolhouse(access_token=st.query_params["th_token"], provider="anthropic")
-        
-    try:
-        available_tools = t.get_tools()
-    except NotFoundException:
-        available_tools = None
-
-    if not available_tools:
-        st.subheader("No tools installed")
-        st.caption(
-            "Go to the [Tool Store](https://app.toolhouse.ai/store) to install your tools, or visit [Bundles](https://app.toolhouse.ai/bundles) to check if the selected bundle exists."
-        )
-    else:
-        st.subheader("Installed tools")
-        for tool in available_tools:
-            tool_name = tool.get("name")
-            st.page_link(f"https://app.toolhouse.ai/store/{tool_name}", label=tool_name)
-
-        st.caption(
-            "\n\nManage your tools in the [Tool Store](https://app.toolhouse.ai/store) or your [Bundles](https://app.toolhouse.ai/bundles)."
-        )
 
 llm = llms.get(llm_choice)
 st.session_state.provider = llm.get("provider")
 model = llm.get("model")
 
 try:
-    th = Toolhouse(access_token=st.query_params["th_token"], provider=llm.get("provider"))
     timezone = st.query_params["tz"] or 0
 
     th.set_metadata("id", st.query_params["th_token"])
@@ -79,10 +68,11 @@ try:
 except Exception as e:
     st.error(f"Invalid API key: {str(e)}")
     st.stop()
+    
+def hide_hero():
+    st.session_state.hide_hero = True
 
-print_messages(st.session_state.messages, st.session_state.provider)
-
-if prompt := st.chat_input("What is up?"):
+def call_llm(prompt):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
@@ -92,7 +82,7 @@ if prompt := st.chat_input("What is up?"):
         model=model,
         messages=st.session_state.messages,
         stream=st.session_state.stream,
-        tools=th.get_tools(bundle=bundle),
+        tools=th.get_tools(),
         max_tokens=4096,
         temperature=0.1,
     ) as response:
@@ -108,7 +98,7 @@ if prompt := st.chat_input("What is up?"):
                 model=model,
                 messages=st.session_state.messages,
                 stream=st.session_state.stream,
-                tools=th.get_tools(bundle=bundle),
+                tools=th.get_tools(),
                 max_tokens=4096,
                 temperature=0.1,
             ) as after_tool_response:
@@ -116,3 +106,18 @@ if prompt := st.chat_input("What is up?"):
                 tool_results = th.run_tools(
                     after_tool_response, append=False
                 )
+
+sidebar()
+hero()
+print_messages(st.session_state.messages, st.session_state.provider)
+
+if st.session_state.prompt is not None:
+    st.session_state.hide_hero = True
+    call_llm(st.session_state.prompt)
+    st.session_state.prompt = None
+
+if prompt := st.chat_input("What is up?", on_submit=hide_hero):
+    st.session_state.prompt = prompt
+    call_llm(prompt)
+    st.session_state.prompt = None
+    
